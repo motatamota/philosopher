1f28560ad752403cb44f2438ad12509664ca8f40
tashiget <tashiget@student.42tokyo.jp>
2024-05-07 10:52:24 +0900

    test

---
+++ philo/includes/philosopher.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tashiget <tashiget@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/07 10:48:04 by tashiget          #+#    #+#             */
+/*   Updated: 2024/05/07 10:48:04 by tashiget         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+# include <stdio.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <string.h>
+
+#endif
